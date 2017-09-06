992f5eb8dd29acf34aa16454c90cfa4f2a23128a
Ajfaust <frostyflakes59@gmail.com>
2017-09-06 11:10:30 -0700

    Changed 'ay' postfix to 'way' for words beginning with vowels

--- platin.py
+++ platin.py
@@ -3,6 +3,6 @@ vowels = ['A', 'a', 'E', 'e', 'I', 'i', 'O', 'o', 'U', 'u']
 def convert(toLang, word):
     if toLang is 'Pig':
         if word[0] is in vowels:
-            return word + 'ay'
+            return word + 'way'
         else:
             return word[1:] + word[0] + 'ay'
