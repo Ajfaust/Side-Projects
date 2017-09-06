26ce3344c5f9e90eb710619a8998f20e4ea33ac0
Ajfaust <frostyflakes59@gmail.com>
2017-09-06 11:12:20 -0700

    Fixed syntax error on line 5

--- platin.py
+++ platin.py
@@ -2,7 +2,7 @@ vowels = ['A', 'a', 'E', 'e', 'I', 'i', 'O', 'o', 'U', 'u']
 
 def convert(toLang, word):
     if toLang is 'Pig':
-        if word[0] is in vowels:
+        if word[0] in vowels:
             return word + 'way'
         else:
             return word[1:] + word[0] + 'ay'
