96aee922e0fc3eb4ae5b02269dd84fafeca0dc32
Ajfaust <frostyflakes59@gmail.com>
2017-09-06 11:18:55 -0700

    Added dash before postfix to make conversions a bit easier

--- platin.py
+++ platin.py
@@ -3,6 +3,7 @@ vowels = ['A', 'a', 'E', 'e', 'I', 'i', 'O', 'o', 'U', 'u']
 def convertTo(toLang, word):
     if toLang is 'Pig':
         if word[0] in vowels:
-            return word + 'way'
+            return word + '-way'
         else:
-            return word[1:] + word[0] + 'ay'
+            return word[1:] + '-' + word[0] + 'ay'
+    elif toLang is 'English':
--- test.py
+++ test.py
@@ -2,9 +2,9 @@ from platin import *
 
 print 'Testing conversion from English to Pig Latin...',
 # Consonants at the beginning
-assert convertTo('Pig', 'Banana') == 'ananaBay'
-assert convertTo('Pig', 'Pineapple') == 'ineapplePay'
+assert convertTo('Pig', 'Banana') == 'anana-Bay'
+assert convertTo('Pig', 'Pineapple') == 'ineapple-Pay'
 # Vowels at the beginning
-assert convertTo('Pig', 'Apple') == 'Appleway'
-assert convertTo('Pig', 'Orange') == 'Orangeway'
+assert convertTo('Pig', 'Apple') == 'Apple-way'
+assert convertTo('Pig', 'Orange') == 'Orange-way'
 print 'OK'
