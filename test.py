9d67ba4f5a95f7e30c1bf353d5033827acefd7f1
Ajfaust <frostyflakes59@gmail.com>
2017-09-06 11:48:24 -0700

    Added tests for Pig Latin to English translations

--- test.py
+++ test.py
@@ -2,9 +2,16 @@ from platin import *
 
 print 'Testing conversion from English to Pig Latin...',
 # Consonants at the beginning
-assert convertTo('Pig', 'Banana') == 'ananaBay'
-assert convertTo('Pig', 'Pineapple') == 'ineapplePay'
+assert convertTo('Pig', 'Banana') == 'ananaBa'
+assert convertTo('Pig', 'Pineapple') == 'ineapplePa'
+assert convertTo('Pig', 'Chocolate') == 'ocolateCha'
+assert convertTo('Pig', 'Steven') == 'evenSta'
 # Vowels at the beginning
 assert convertTo('Pig', 'Apple') == 'Appleway'
 assert convertTo('Pig', 'Orange') == 'Orangeway'
 print 'OK'
+
+print 'Testing conversion from Pig Latin to English...',
+assert convertTo('English', 'ananaBa') == 'Banana'
+assert convertTo('English', 'ineapplePa') == 'Pineapple'
+assert convertTo('English', 'Appleway') == 'Apple'
