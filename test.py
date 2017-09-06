e6dc0bed80873381264713cd395b4282bc9971fd
Ajfaust <frostyflakes59@gmail.com>
2017-09-06 11:11:44 -0700

    Added tests for words beginning with a vowel

--- test.py
+++ test.py
@@ -1,6 +1,10 @@
 from platin import *
 
 print 'Testing conversion from English to Pig Latin...',
+# Consonants at the beginning
 assert convert('Pig', 'Banana') == 'ananaBay'
 assert convert('Pig', 'Pineapple') == 'ineapplePay'
+# Vowels at the beginning
+assert convert('Pig', 'Apple') == 'Appleway'
+assert convert('Pig', 'Orange') == 'Orangeway'
 print 'OK'
