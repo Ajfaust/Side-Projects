2e11353707430f90c6221bd05078fdefbc53c188
Ajfaust <frostyflakes59@gmail.com>
2017-09-07 15:38:40 -0700

    Changed appropriate function names. Deleted tests for english conversion

--- Pig-Latin/test.py
+++ Pig-Latin/test.py
@@ -2,17 +2,12 @@ from platin import *
 
 print 'Testing conversion from English to Pig Latin...',
 # Consonants at the beginning
-assert convertTo('Pig', 'Banana') == 'ananaBa'
-assert convertTo('Pig', 'Pineapple') == 'ineapplePa'
-assert convertTo('Pig', 'Chocolate') == 'ocolateCha'
-assert convertTo('Pig', 'Steven') == 'evenSta'
+assert toPLatin('Pig', 'Banana') == 'ananaBa'
+assert toPLatin('Pig', 'Pineapple') == 'ineapplePa'
+assert toPLatin('Pig', 'Chocolate') == 'ocolateCha'
+assert toPLatin('Pig', 'Steven') == 'evenSta'
 # Vowels at the beginning
-assert convertTo('Pig', 'Apple') == 'Appleway'
-assert convertTo('Pig', 'Orange') == 'Orangeway'
+assert toPLatin('Pig', 'Apple') == 'Appleway'
+assert toPLatin('Pig', 'Orange') == 'Orangeway'
 print 'OK'
 
-print 'Testing conversion from Pig Latin to English...',
-assert convertTo('English', 'ananaBa') == 'Banana'
-assert convertTo('English', 'ineapplePa') == 'Pineapple'
-assert convertTo('English', 'Appleway') == 'Apple'
-print 'OK'
