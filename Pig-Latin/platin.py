c7e50ee4521e6ad76898acdda0f037820ef5eb30
Ajfaust <frostyflakes59@gmail.com>
2017-09-07 15:36:48 -0700

    Changed convertTo to toPLatin.
    Deleted code for English conversion for now.

--- Pig-Latin/platin.py
+++ Pig-Latin/platin.py
@@ -4,15 +4,8 @@ def findFirstVowel(word):
         if word[i].lower() in vowels:
             return i
 
-def convertTo(toLang, word):
-    if toLang is 'Pig':
-        fv = findFirstVowel(word)
-        if fv is 0:
-            return word + 'way'
-        return word[fv:] + word[:fv] + 'a'
-        
-    elif toLang is 'English':
-        if word[-3:] == 'way':
-            return word[:-3]
-        else:
-            return word[-2] + word[:-2]
+def toPLatin(word):
+    fv = findFirstVowel(word)
+    if fv is 0:
+        return word + 'way'
+    return word[fv:] + word[:fv] + 'a'
