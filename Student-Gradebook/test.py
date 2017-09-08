6cef65998643c0c1dd6546d4f2ea45fdd054943b
Ajfaust <frostyflakes59@gmail.com>
2017-09-08 10:56:21 -0700

    Added initial test Class, and tests for average calculation

--- Student-Gradebook/test.py
+++ Student-Gradebook/test.py
@@ -1,6 +1,24 @@
 # A collection of tests for gradebook
 from gradebook import *
 
+# Initialize a small class to run tests on
+print 'Initializing test Class...',
+c = Class("Biology", 83732, {})
+names = ['Andrew', 'Mary', 'Steven',  'Alex', 'John']
+for name in names:
+    c.addStudent(name)
+print 'Done'
+
 # Test if string output for student works
-s = Student("Andrew", 0, [])
-assert s.__str__() == 'Name: Andrew\nAverage: 0\nRecent scores: '
+print 'Testing Student string output...',
+assert str(c.students['Andrew']) == 'Name: Andrew\nAverage: 0\nRecent scores: '
+print 'OK'
+
+# Test if average calculation works
+print 'Testing average calculation...',
+scores = [48, 91, 76, 86, 77, 95]
+for score in scores:
+    c.students['Mary'].addScore(score)
+c.students['Mary'].calcAvg()
+assert c.students['Mary'].avg == 473 / 600
+print 'OK'
