43350e4b0e151c0a52de5eae0b8b9c10bb45b2c6
ajfaust <frostyflakes59@gmail.com>
2017-09-07 21:45:25 -0700

    Fixed previous tests to account for function name changes. Added tests for phrase translations

--- Pig-Latin/test.py
+++ Pig-Latin/test.py
@@ -1,13 +1,30 @@
 from platin import *
+import sys
+import traceback
 
-print 'Testing conversion from English to Pig Latin...',
+print 'Testing conversion of words to Pig Latin...',
 # Consonants at the beginning
-assert toPLatin('Pig', 'Banana') == 'ananaBa'
-assert toPLatin('Pig', 'Pineapple') == 'ineapplePa'
-assert toPLatin('Pig', 'Chocolate') == 'ocolateCha'
-assert toPLatin('Pig', 'Steven') == 'evenSta'
+assert toPLatin('Banana') == 'ananaBa'
+assert toPLatin('Pineapple') == 'ineapplePa'
+assert toPLatin('Chocolate') == 'ocolateCha'
+assert toPLatin('Steven') == 'evenSta'
 # Vowels at the beginning
-assert toPLatin('Pig', 'Apple') == 'Appleway'
-assert toPLatin('Pig', 'Orange') == 'Orangeway'
+assert toPLatin('Apple') == 'Appleway'
+assert toPLatin('Orange') == 'Orangeway'
 print 'OK'
 
+print 'Testing conversion of phrases to Pig Latin...',
+phrases = ['Hello world',\
+            'This is pig latin',\
+            'Some sort of longer phrase']
+pl_phrases = ['elloHa orldwa',\
+            'isTha isway igpa atinla',\
+            'omeSa ortsa ofway ongerla asephra']
+for pIndex in range(len(phrases)):
+    try:
+        assert convertPhrase(phrases[pIndex]) == pl_phrases[pIndex]
+    except AssertionError:
+        _,_,tb = sys.exc_info()
+        traceback.print_tb(tb)
+        print ('Error for phrase \'' + phrases[pIndex] + '\'.\n')
+print 'OK'
