638bb991e67ddee714c971c53b777c3b577bef6e
Ajfaust <frostyflakes59@gmail.com>
2017-09-10 17:39:52 -0700

    Added assign_grades_norm function to assign grades on a normal scale.
    A normal scale being:
        90-100 = A
        80-89 = B
        70-79 = C
        etc.

--- Student-Gradebook/gradebook.py
+++ Student-Gradebook/gradebook.py
@@ -11,17 +11,20 @@
 # @name: Student name
 # @avg: Student's running average
 # @scores: List of student's scores per assignment
+# @grade: Student grade
 class Student:
     def __init__(self, name, scores=[]):
         self.name = name
         self.avg = 0.0 if scores == [] else self.calcAvg(scores)
         self.scores = scores
+        self.grade = "N/A"
 
     def __str__(self):
         return 'Name: ' + self.name\
             + '\nAverage: ' + str(self.avg)\
             + '\nRecent scores: ' + ', '.join([str(s) for s in
-                self.scores[-5:]])
+                self.scores[-5:]])\
+            + '\nCurrent grade: ' + self.grade
 
     def calcAvg(self, scores):
         return sum(self.scores) / (100 * len(self.scores))
@@ -49,3 +52,18 @@ class Class:
     def printStudentInfo(self, students):
         for student in self.students:
             return str(student)
+
+    def assign_grades_norm(self, students):
+        for student in student:
+            if student.scores == []:
+                student.grade = "N/A"
+            elif student.avg >= 90:
+                student.grade = "A"
+            elif student.avg >= 80:
+                student.grade = "B"
+            elif student.avg >= 70:
+                student.grade = "C"
+            elif student.avg >= 60:
+                student.grade = "D"
+            else:
+                student.grade = "F"
