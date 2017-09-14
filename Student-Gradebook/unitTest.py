29ab4be66b4427fd58a84fb5ba5bd1d8052f6529
ajfaust <frostyflakes59@gmail.com>
2017-09-13 19:30:19 -0700

    Added in a unit test file to replace test.py

---
+++ Student-Gradebook/unitTest.py
@@ -0,0 +1,38 @@
+import unittest
+from gradebook import Student, Course
+
+class TestStudentMethods(unittest.TestCase):
+    def setUp(self):
+        self.s = Student('Andrew', [56, 78, 98, 90, 94, 87])
+
+    def testAvg(self):
+        self.assertAlmostEqual(self.s.avg, 83.83, places=2)
+    
+    def testScores(self):
+        self.failUnless(self.s.scores == [56, 78, 98, 90, 94, 87])
+    
+    def testGrade(self):
+        self.failUnless(self.s.calcGrade(-1, -1) == "B")
+    
+    def testAddScore(self):
+        self.s.addScore(67)
+        self.failUnless(self.s.scores == [56, 78, 98, 90, 94, 87, 67])
+        self.assertAlmostEqual(self.s.avg, 81.43, places=2)
+
+class TestCourseMethods(unittest.TestCase):
+    def setUp(self):
+        self.c = Course("Biology", 48364, {'Andrew': [56, 78, 98, 90, 94, 87],
+                                            'Mary': [48, 91, 76, 86, 77, 95],
+                                            'John': [67, 56, 65, 45, 76, 55]})
+        self.c.updateGrades()
+    
+    def testGrades(self):
+        self.failUnless(self.c.students['Andrew'].grade == "B")
+        self.failUnless(self.c.students['Mary'].grade == "C")
+        self.failUnless(self.c.students['John'].grade == "D")
+
+def main():
+    unittest.main()
+
+if __name__ == '__main__':
+    main()
